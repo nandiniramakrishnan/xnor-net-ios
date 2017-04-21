9aa1242e35628b42fdc7eab1304ebb8a2ae9e507
nandiniramakrishnan <nramakri@andrew.cmu.edu>
2017-04-20 20:59:41 -0400

    Armadillo matrix multiplication

--- xnor/xnor/ViewController.mm
+++ xnor/xnor/ViewController.mm
@@ -8,6 +8,12 @@
 
 #import "ViewController.h"
 
+#ifdef __cplusplus
+#include "armadillo"
+#include <stdlib.h>
+
+#endif
+
 @interface ViewController ()
 
 @end
@@ -17,6 +23,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    using namespace arma;
+
+    int D = 3000; // Number of columns in A
+    int M = 400; // Number of rows in A
+    int trials = 3000; // Number of trials
+    arma::wall_clock timer;
+    
+    
+    // initialize random data
+    arma::fmat x; x.randn(D,1);
+    arma::fmat A; A.randn(M,D);
+    // intialize the clock
+    
+    
+    
+    timer.tic();
+    // Step 4. apply matrix multiplication in Armadillo
+    arma::fmat y(M,1); // Allocate space first
+    timer.tic();
+    for(int i=0; i<trials; i++) {
+        y = A*x; // Apply multiplication in Armadillo
+    }
+    double arma_n_secs = timer.toc();
+    cout << "Armadillo took " << arma_n_secs << " seconds." << endl;
+    
 }
 
 
---
+++ xnor/xnor/multiply.hpp
@@ -0,0 +1,17 @@
+//
+//  multiply.hpp
+//  xnor
+//
+//  Created by Nandini Ramakrishnan on 10/04/17.
+//  Copyright © 2017 Shalini Ramakrishnan. All rights reserved.
+//
+
+#ifndef multiply_hpp
+#define multiply_hpp
+
+#include <stdio.h>
+// armadillo_multiply(fmat W, fvec, x)
+
+// binary_xnor(mat W, vec x)
+
+#endif /* multiply_hpp */
